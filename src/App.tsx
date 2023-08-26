7e3fdd1df07966d7d4b0873627f52b55d79ba7f3
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-08-26 18:54:19 +0900

    The first page.
    + Hello World test

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: fantiga
+ * @Date: 2023-08-26 18:44:59
+ * @LastEditTime: 2023-08-26 18:53:40
+ * @LastEditors: fantiga
+ * @FilePath: /population-change-map-react-ts/src/App.tsx
+ */
+
+import React from "react";
+import { FC } from "react";
+
+const App: FC = () => (
+  <React.StrictMode>
+    Hello World!
+  </React.StrictMode>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: fantiga
+ * @Date: 2023-08-26 18:38:38
+ * @LastEditTime: 2023-08-26 18:44:43
+ * @LastEditors: fantiga
+ * @FilePath: /population-change-map-react-ts/src/index.tsx
+ */
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+/**
+ * React 18 新写法
+ */
+const container: HTMLElement = document.getElementById('root') || document.createElement('div');
+// 创建一个root。
+const root = createRoot(container);
+// 初始渲染：将一个元素渲染到root。
+root.render(<App />);
