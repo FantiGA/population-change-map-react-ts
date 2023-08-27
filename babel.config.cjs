551e5de3a2e7b3bb7f16bbdcf504a18f9e1b61dc
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-08-27 21:35:03 +0900

    Add jest module

---
+++ babel.config.cjs
@@ -0,0 +1,19 @@
+/*
+ * @Author: fantiga
+ * @Date: 2023-08-27 21:34:25
+ * @LastEditTime: 2023-08-27 21:34:26
+ * @LastEditors: fantiga
+ * @FilePath: /population-change-map-react-ts/babel.config.cjs
+ */
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current"
+        }
+      }
+    ]
+  ]
+};
